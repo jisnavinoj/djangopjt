4ea7a69750c453f734d31f197390edd8ace482cf
jisnavinoj <jisnajohnichan1997@gmail.com>
2023-01-17 16:01:52 +0530

    commit

---
+++ products/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Product, offer
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
+class offerAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
+
+
+# Register your models here.
+admin.site.register(Product, ProductAdmin)
+admin.site.register(offer, offerAdmin)
---
+++ products/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+
+# Create your views here.
+def index(request):
+    products=Product.objects.all()
+    return render(request,'index.html',{'products':products})
+    #return HttpResponse("<h1>welcome</h1>")
+
+def about(request):
+    return HttpResponse("<h1>about page</h1>")
+
+
